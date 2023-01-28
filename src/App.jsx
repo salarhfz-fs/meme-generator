e14ddfb40a366c008842bd8df1c88a8ab713f5aa
Salar Hafezi <salar.hfz.work@gmail.com>
2023-01-28 20:30:25 +0330

    add components to app - finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
+import { useEffect, useReducer, useState } from 'react'
+
+import { getRandomIntegerInRange } from './common/utils'
+import Header from './components/header'
+import Form from './components/form'
+import Meme from './components/meme'
+
 import './App.scss'
 
+const initialState = {
+  memes: [],
+  currentMeme: null,
+  topText: '',
+  bottomText: ''
+}
+
+function reducer(state, action) {
+  const { type, payload } = action
+
+  switch (type) {
+    case 'memes':
+      return { ...state, memes: payload }
+    case 'currentMeme':
+      return { ...state, currentMeme: payload }
+    case 'topText':
+      return { ...state, topText: payload }
+    case 'bottomText':
+      return { ...state, bottomText: payload }
+    default:
+      return state
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    async function getMemes() {
+      const response = await fetch('https://api.imgflip.com/get_memes')
+      const responseJson = await response.json();
+      if (responseJson?.success) {
+        dispatch({ type: 'memes', payload: responseJson.data?.memes })
+      }
+    }
+    getMemes()
+  }, [])
+
+  useEffect(() => {
+    updateMeme()
+  }, [state.memes])
+
+  function updateMeme() {
+    const randomInteger = getRandomIntegerInRange(0, state.memes?.length - 1)
+    dispatch({ type: 'currentMeme', payload: state.memes[randomInteger] })
+  }
+
   return (
-    <h1>App</h1>
+    <div className="container">
+      <Header />
+      <Form
+        setup={state.topText}
+        punchline={state.bottomText}
+        dispatch={dispatch}
+        onMemeChange={updateMeme}
+      />
+      <Meme
+        topText={state.topText}
+        bottomText={state.bottomText}
+        memeUrl={state.currentMeme?.url}
+      />
+    </div>
   )
 }
 
